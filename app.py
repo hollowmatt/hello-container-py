c019a81fd582e2f83c81a2753f84f9893ad1794c
Matt Holloway <matthew.holloway@logicwerx.com>
2022-01-14 15:58:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World, from a GitHub Pull Reqeust!"
+    return "Hello World, from a GitHub Pull Reqeust, for realz!"
 
 
 if __name__ == "__main__":
